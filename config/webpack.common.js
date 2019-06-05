7b3fe07113a42224588f12802598b0827d8ebcc9
Michael Badeen <Michael.Badeen@autozone.com>
2019-06-05 17:06:57 -0400

    WIP

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -5,7 +5,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: {
-    app: './src/index.html'
+    app: './src/index.js'
   },
   output: {
     filename: '[name].bundle.js',
